962b27ad1a2ffa87cbf21d8375f0cd24cd1b38ab
Tisha Greenidge <tisha.m.greenidge@gmail.com>
2015-08-14 07:33:40 -0400

    stringifyJSON incomplete with 1 test failing; getElementsByClassName incomplete

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,48 @@
 // };
 
 // But instead we're going to implement it from scratch:
+// 
+//nodes = Array.prototype.slice.apply(document.body.childNodes); // nodes is a collectio
+
+
+nodes = Array.prototype.slice.apply(document.body.childNodes);
 var getElementsByClassName = function(className
 ){
+  // use document.body, element.childNodes, element.classList
+  // use childnodes to get info
+  //"The Element.classList read-only property returns a live DOMTokenList of the class attributes of the element."
+  //"Using classList is a convenient alternative to accessing an element's list of classes as a space-delimited string via element.className."
   // your code here
+  
+ //  	nodes = nodes || document.body;
+	// nodesWithClassName =  (nodes[i].classList).contains(className) ;
+	// elementsWithClassName = elementsWithClassName || [];
+
+	// classNames = document.body.classList;
+    
+ //    for (var i = 0; i < nodes.length; i++){
+    	
+ //    	//elementsWithClassName = nodesWithClassName.tagname;
+ //    }
+
+  //   if(classNames.contains(className)){
+  //  elementsWithClassName.push(nodes);
+  // }
+  	
+
+	
+	// classNames = document.body.classList;
+	// console.log("CN", classNames);
+ //  	console.log("NL", nodes);
+  
+  // Is className in names on document?
+
+
+  //get elements in nodes
+
+  //return getElementsByClassName(nodes.remove);
+  return nodesWithClassName;
 };
+
+
+
--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,4 +5,85 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
+  
+  //primitive types
+  /* 
+  string
+number
+boolean
+null
+undefined
+symbol (new in ECMAScript 2015)
+*/
+	if(obj === null){
+		return 'null';
+	}
+
+
+   if (['number','boolean'].indexOf(typeof obj) !== -1){
+      return String(obj);
+    } 
+
+    if(typeof obj === 'string'){
+    	first = '"';
+   		last = '"';
+   		return  first + obj + last;
+    }
+
+    else if (['function', 'undefined', 'symbol'].indexOf(typeof obj) !== -1) {
+      
+   } else if( Array.isArray(obj)){
+   		firstStrArray = "[" ;
+   		lastStrArray = "]";
+   		if (obj.length === 0){
+   			return '[]';
+   		}else {
+   			for(var i = 0; i < obj.length; i++){
+   				if(typeof obj[i] === 'number'){
+   				}else{
+   				obj[i] = stringifyJSON(obj[i]);
+   			}
+   			
+   		}
+   			return firstStrArray + obj + lastStrArray;	
+   		}
+        
+   } else {
+    
+        
+   		
+     	if (Object.keys(obj).length === 0){
+     		return '{}';
+     	} else {
+     		objArray = []; // store keys and values in array
+   			var y = 0;  //ObjArrays incrementor
+     		//objArray.length = Object.keys(obj).length;
+     		firstStrObj = "{";
+     		lastStrObj = "}";
+
+     		for (key in obj){
+          
+          if(typeof obj[key] === 'undefined' || typeof obj[key] === 'function' || typeof obj[key] === 'symbol'){
+            return '{}';
+            
+          } else if( Object.keys(obj).length === 1 ){
+             if(typeof obj[key] === 'string' ){
+                objArray[y]= stringifyJSON(key) + ":" + stringifyJSON(obj[key] ); 
+              } else {
+                objArray[y]= stringifyJSON(key) + ":" + obj[key] ;
+                // not sure how to access objects like ('{"a":{"b":"c"}}')
+              }
+        
+
+          } else {
+          objArray[y]= stringifyJSON(key) + ":" + stringifyJSON(obj[key]) ;       
+          
+          } //else
+           y ++;  
+        } //for
+          return firstStrObj + objArray + lastStrObj;
+} //else obj is an object 
+}
+   
 };
+
