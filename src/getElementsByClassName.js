f12e6cbe01f2340bab694b07a78115b505e809d8
Tisha Greenidge <tisha.m.greenidge@gmail.com>
2015-08-15 08:38:20 -0400

    completed getElementsByClassName

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -5,46 +5,48 @@
 
 // But instead we're going to implement it from scratch:
 // 
-//nodes = Array.prototype.slice.apply(document.body.childNodes); // nodes is a collectio
 
-
-nodes = Array.prototype.slice.apply(document.body.childNodes);
-var getElementsByClassName = function(className
-){
+var getElementsByClassName = function(className){
   // use document.body, element.childNodes, element.classList
   // use childnodes to get info
   //"The Element.classList read-only property returns a live DOMTokenList of the class attributes of the element."
   //"Using classList is a convenient alternative to accessing an element's list of classes as a space-delimited string via element.className."
   // your code here
-  
- //  	nodes = nodes || document.body;
-	// nodesWithClassName =  (nodes[i].classList).contains(className) ;
-	// elementsWithClassName = elementsWithClassName || [];
-
-	// classNames = document.body.classList;
-    
- //    for (var i = 0; i < nodes.length; i++){
-    	
- //    	//elementsWithClassName = nodesWithClassName.tagname;
- //    }
-
-  //   if(classNames.contains(className)){
-  //  elementsWithClassName.push(nodes);
-  // }
-  	
-
-	
-	// classNames = document.body.classList;
-	// console.log("CN", classNames);
- //  	console.log("NL", nodes);
-  
-  // Is className in names on document?
 
 
-  //get elements in nodes
 
-  //return getElementsByClassName(nodes.remove);
-  return nodesWithClassName;
+	 var elementsWithClassName = [];
+
+	  getMoreWithClassName(document.childNodes);
+
+	  
+	  function getMoreWithClassName(childrenNodes) {
+	    for (var i = 0; i < childrenNodes.length; i++) {
+	      if (hasClassName(childrenNodes[i])) {
+	        elementsWithClassName.push(childrenNodes[i]); 
+	      }
+	      if (childrenNodes[i].childNodes) {
+	        getMoreWithClassName(childrenNodes[i].childNodes); 
+	      }
+	    }
+	  }
+
+	  function hasClassName(node) {
+	    var nodeClasses = node.classList;
+	    
+	    var foundClassName = false;
+	    if (nodeClasses) {
+	      for (var i = 0; i < nodeClasses.length; i++) {
+	        if (className == nodeClasses[i]) {
+	          foundClassName = true;
+	        }
+	      }
+	    }
+	    return foundClassName;
+	  }
+	  return elementsWithClassName;
+
+		
 };
 
 
